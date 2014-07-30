3582fafd04ec6b40fda7049a87d80cadd87c3410
Sean Girard <seangirard@yahoo.com>
2014-07-30 16:05:06 -0700

    widget

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '258f6a6f0c547e255f9420a81b2c4708');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f892dabe8fd2c7d89e3c9c10b8b94cbf');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
