b9f6544593e67efad0847f3fb8d5316e226dfd0b
Sean Girard <seangirard@yahoo.com>
2014-07-30 14:29:22 -0700

    breakpoints

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '3ab63f33072d071aa173b995363addca');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ea138b67ef5e0f67cf476cb41cea594e');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
