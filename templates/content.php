8cd4bde9ebc9764d579e944406aaeecdec3f2d78
Sean Girard <seangirard@yahoo.com>
2014-07-31 08:55:15 -0700

    title

--- templates/content.php
+++ templates/content.php
@@ -1,5 +1,5 @@
 <article <?php post_class(); ?>>
-  <?php if ( the_title() ) : ?>
+  <?php if ( get_the_title() ) : ?>
   <header>
     <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <?php get_template_part('templates/entry-meta'); ?>
