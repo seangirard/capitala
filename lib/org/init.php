87fd3f4d017aba8d652d64f34c34a07752866d0a
Sean Girard <seangirard@yahoo.com>
2014-07-31 11:21:33 -0700

    port

--- lib/org/init.php
+++ lib/org/init.php
@@ -53,9 +53,9 @@ function capitala_portfolio($params) {
     , 'order' => 'ASC'
   );  
   $posts = new WP_query($args);  
-  $html .= print_r($posts);
-  //$count = count($posts[->]post_count);
-  //$html .= $count;
+  //$html .= print_r($posts);
+  $count = count($posts['post_count']);
+  $html .= $count;
   if ($posts->have_posts()) : 
     $html .= '<div class="row">'.PHP_EOL;
     $i = 1;
