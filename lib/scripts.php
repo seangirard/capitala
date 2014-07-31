a2e09f5068485f2e1e728ea622952fe92f750d2d
Sean Girard <seangirard@yahoo.com>
2014-07-31 09:21:54 -0700

    blockquote

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '11951882bc66c18355641dbaad35832d');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5a5619372d6caac51b46b6d531c1dc1d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
