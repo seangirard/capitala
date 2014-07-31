6da9e95c5ea65815a3f9a35bc556e0a863b4190a
Sean Girard <seangirard@yahoo.com>
2014-07-31 09:13:27 -0700

    titles

--- templates/entry-meta.php
+++ templates/entry-meta.php
@@ -1,4 +1,4 @@
-<div class="text-muted">
+<div class="text-muted" style="margin-bottom: 10px;">
 <time class="published pull-right" datetime="<?php echo get_the_time('c'); ?>"><b><?php echo get_the_date(); ?></b></time>
 <span class="byline author vcard"><?php echo __('by', 'roots'); ?> <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>" rel="author" class="fn"><?php echo get_the_author(); ?></a></span>
 </div>
