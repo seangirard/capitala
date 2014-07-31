d4925ff31c289cef43584afa4834b4b8c53a2f43
Sean Girard <seangirard@yahoo.com>
2014-07-31 09:04:28 -0700

    color

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '16ce7f1bb4b7990146e665b4422fd8ce');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '757ea1123ee12b1d49b02eacb743d487');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
--- templates/content.php
+++ templates/content.php
@@ -1,12 +1,8 @@
 <article <?php post_class(); ?>>
-  <?php 
-  $title = get_the_title();
-  if ( isset($title) ) : ?>
   <header>
     <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
- 	<?php endif; ?>
   <div class="entry-summary clearfix">
     <?php //the_excerpt(); ?>
     <?php the_content(); ?>
