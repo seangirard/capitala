41e32e23d5d0a0581ec9c4c113466ba0f03af820
Sean Girard <seangirard@yahoo.com>
2014-07-31 09:57:26 -0700

    header

--- templates/org/header-top-navbar.php
+++ templates/org/header-top-navbar.php
@@ -1,44 +1,3 @@
-<!--<header class="navbar-common banner navbar navbar-fixed-top" role="navigation">
-  <ul class="nav nav-justified">
-    <li>
-      <a href="<?php echo home_url(); ?>/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-home"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-home"></i> Home</span>
-      </a>
-    </li>
-    <li>
-      <a href="/account/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-user"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-user"></i> My Account</span>
-      </a>
-    </li>
-    <li>
-      <a href="/search/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-search"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-search"></i> Search</span>
-      </a>
-    </li>
-    <li>
-      <a href="/browse/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-book"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-book"></i> Browse</span>
-      </a>
-    </li>
-    <li>
-      <a href="/download/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-download-alt"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-download-alt"></i> Download</span>
-      </a>
-    </li>
-    <li>
-      <a href="/connect/">
-        <span class="visible-xs"><i class="glyphicon glyphicon-envelope"></i></span>
-        <span class="hidden-xs"><i class="text-success glyphicon glyphicon-envelope"></i> Contact Us</span>
-      </a>
-    </li>
-  </ul>
-</header>--><!-- /.navbar -->
-
 <header class="navbar-masthead banner navbar navbar-inverse navbar-static-top visible-md visible-lg" style="margin-top:20px;" role="banner">
   <div class="container">
     <div class="navbar-header">
@@ -59,7 +18,8 @@
         <span class="icon-bar"></span>
       </button>
       <a class="navbar-brand hidden-md hidden-lg" href="<?php echo home_url(); ?>/">
-        <span class="logotype"><?php bloginfo('name'); ?></span>
+        <span class="logotype hidden-sm"><?php bloginfo('name'); ?></span>
+        <span class="logotype hidden-md">Capital A Pub.</span>
       </a>
     </div>
     <nav class="collapse navbar-collapse" role="navigation">
