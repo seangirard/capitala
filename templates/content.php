a15fca6f8fc39277054b1666682febafa8f38f45
Sean Girard <seangirard@yahoo.com>
2014-07-31 08:52:16 -0700

    tiles

--- templates/content.php
+++ templates/content.php
@@ -1,8 +1,10 @@
 <article <?php post_class(); ?>>
+  <?php if ( !empty(the_title()) ) : ?>
   <header>
     <h1 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
     <?php get_template_part('templates/entry-meta'); ?>
   </header>
+ 	<?php endif; ?>
   <div class="entry-summary clearfix">
     <?php //the_excerpt(); ?>
     <?php the_content(); ?>
