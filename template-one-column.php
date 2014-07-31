daccd237e109c1d0f94cb55a5b505575563c0e31
Sean Girard <seangirard@yahoo.com>
2014-07-31 12:01:25 -0700

    single column

--- template-one-column.php
+++ template-one-column.php
@@ -7,7 +7,7 @@ Template Name: A Single Column (No Sidebar)
 
 <div class="row">
 
-	<div class="col-md-10 col-lg-9">
+	<div class="col-sm-12">
 		<?php get_template_part('templates/page', 'header'); ?>
 		<?php get_template_part('templates/content', 'page'); ?>
 	</div>
